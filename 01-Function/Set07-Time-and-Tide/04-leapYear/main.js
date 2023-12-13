318b2159be6afcd36daf13f8239bedbdf1093a5d
pavitpim40_mbp <pavitpim@gmail.com>
2023-12-13 12:18:38 +0700

    update:isLeapYear

--- 01-Function/Set07-Time-and-Tide/04-leapYear/main.js
+++ 01-Function/Set07-Time-and-Tide/04-leapYear/main.js
@@ -2,15 +2,58 @@
 // - leap year คือปี ค.ศ. ที่หารด้วย 4 ลงตัว
 // - ยกเว้นปีที่เป็นผลคูณของ 100 เช่น 1800, 1900, 2000 จะต้องหารด้วย 400 ลงตัว
 
-// 4,8,12 => true
-// 100,200,300 => false
-// 400 => true
+/* 
+S1 : UnderStand Problem 
+            A -----------> B
+year <number> -----------> isLeapYear <boolean>
 
-// 1yr = 365.242199
-// 1yr = 365 (เหลือ .242199)
-// 4yr = 365 + 4*(0.242199) = 365.968... = 366
+S2 : Explore Example
+// หาร 4 ลงตัวเป็น leapYear (easiest)
+4 ====> true
+8 ====> true
+80 ====> true
 
-// 100yr = 365
-// 200yr = 365
-// 300yr = 365
-// 400yr = 366
+// หาร 400 ลงตัวเป็น leapYear 
+400 ===> true
+800 ===> true
+
+// หาร 100 ลงตัว และ หาร 400 ไม่ลงตัว (complex)
+100 ==> false
+200 ==> false 
+400 ==> true
+
+// S3 : Break Problem 
+// S4 : Solve / Simplify (Back to S3)
+*/
+
+// function isLeapYear(year) {
+//   if (year % 100 == 0 && year % 400 !== 0) return false;
+//   if (year % 400 == 0) return true;
+//   if (year % 4 == 0) return true;
+//   return false;
+// }
+
+// function isLeapYear(year) {
+//   if (year % 100 == 0 && year % 400 !== 0) return false;
+//   else if (year % 400 == 0) return true;
+//   else if (year % 4 == 0) return true;
+//   return false;
+// }
+
+// function isLeapYear(year) {
+//   if (year % 400 == 0) return true;
+//   else if (year % 100 == 0) return false;
+//   else if (year % 4 == 0) return true;
+//   return false;
+// }
+
+function isLeapYear(year) {
+  // if (year % 400 == 0) return true;
+  // else if (year % 100 == 0) return false;
+  // else if (year % 4 == 0) return true;
+  // return false;
+
+  return year % 400 == 0 || (year % 4 == 0 && year % 100 !== 0);
+
+  // return year % 400 == 0  ? year % 100 != 0 : year % 4 == 0
+}
